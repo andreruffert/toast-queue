1f4565188fe4a84e70962da7b89622a271254543
André Ruffert <464300+andreruffert@users.noreply.github.com>
2025-10-23 21:22:56 +0200

    feat: alpha iteration

--- src/toast-queue.test.js
+++ src/toast-queue.test.js
@@ -1,5 +1,5 @@
 import { expect, test, vi } from 'vitest';
-// import { page } from 'vitest/browser';
+import { page } from 'vitest/browser';
 import { ToastQueue } from './toast-queue.js';
 
 test('defaults', async () => {
@@ -30,39 +30,39 @@ test('renders a toast message', async () => {
     onClose: onCloseCallback,
   });
 
-  // const popoverElement = page.getByRole('region', { name: '1 notification' }).element()
-  const popoverElement = document.querySelector('[data-tq-part="popover"]');
-  // const toastElement = page.getByRole('alertdialog').element();
-  const toastElement = document.querySelector('[data-tq-part="toast"]');
-  // const toastContent = page.getByRole('alert').element();
-  const toastContent = document.querySelector('[data-tq-part="content"]');
-  // const closeButton = page.getByRole('button', { name: 'Close' }).element();
-  const closeButton = document.querySelector('[data-tq-part="close-button"]');
+  // const popoverElement = page.getByRole('region', { name: '1 notification' }).element();
+  // const popoverElement = document.querySelector('[data-tq-part="popover"]');
+  const toastElement = page.getByRole('alertdialog');
+  // const toastElement = document.querySelector('[data-tq-part="toast"]');
+  const toastContent = page.getByRole('alert');
+  // const toastContent = document.querySelector('[data-tq-part="content"]');
+  const closeButton = page.getByRole('button', { name: 'Close' });
+  // const closeButton = document.querySelector('[data-tq-part="close-button"]');
 
   await expect.element(toastElement).toBeInTheDocument();
   await expect.element(toastContent).toBeInTheDocument();
   await expect.element(closeButton).toBeInTheDocument();
 
-  expect(popoverElement).toHaveAttribute('aria-label', '1 notification');
+  // expect(popoverElement).toHaveAttribute('aria-label', '1 notification');
   expect(toastElement).toHaveAttribute('tabindex', '0');
-  expect(toastElement).toHaveAttribute('role', 'alertdialog');
+  // expect(toastElement).toHaveAttribute('role', 'alertdialog');
   expect(toastElement).toHaveAttribute('aria-modal', 'false');
-  expect(toastElement).toHaveAttribute('aria-labelledby', `aria-label-${toastRef.id}`);
-  // expect(toastElement).toHaveAccessibleName(toastRef.content?.title || toastRef.content);
-  // expect(toastElement).toHaveAccessibleDescription(toastRef.content?.description || '');
+  // expect(toastElement).toHaveAttribute('aria-labelledby', `aria-label-${toastRef.id}`);
+  expect(toastElement).toHaveAccessibleName(toastRef.content?.title || toastRef.content);
+  expect(toastElement).toHaveAccessibleDescription(toastRef.content?.description || '');
   expect(toastElement).toHaveAttribute('data-tq-id', toastRef.id);
   expect(toastElement).toHaveAttribute('data-tq-part', 'toast');
   expect(toastElement).toHaveAttribute('data-tq-dismissible', 'true');
 
-  expect(toastContent).toHaveAttribute('role', 'alert');
+  // expect(toastContent).toHaveAttribute('role', 'alert');
   expect(toastContent).toHaveAttribute('aria-atomic', 'true');
 
-  expect(closeButton).toHaveAttribute('aria-label', 'Close');
+  // expect(closeButton).toHaveAttribute('aria-label', 'Close');
 
   await closeButton.click();
   expect(onCloseCallback).toHaveBeenCalled();
   // expect(popoverElement).toHaveAccessibleName('0 notifications');
-  expect(popoverElement).toHaveAttribute('aria-label', '0 notifications');
+  // expect(popoverElement).toHaveAttribute('aria-label', '0 notifications');
 
   toastQueue.unmount();
 });
